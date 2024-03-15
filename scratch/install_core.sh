1793b6d34fef2836251710e55980bdd205e845ff
Tim Hurton <timhurton@gmail.com>
2024-03-15 11:18:34 +0000

    Trying to ignore folders

--- scratch/install_core.sh
+++
@@ -1,3 +0,0 @@
-# Not sure yet how we rae going to manage parameterising the schema name for different environments.
-psql -d neowms -h 10.5.2.4 -p 5432 -U dblidl -f recreate.sql > $stdout/recreate.lst 2> $error_log/recreate.err 
-cat $error_log/recreate.err
--- scratch/rebuild_neowms.sh
+++
@@ -1,4 +0,0 @@
-psql -d neowms -h 10.5.2.4 -p 5432 -U dblidl -f neowms_tables.sql > $stdout/neowms_tables.lst 2> $error_log/neowms_tables.err 
-cat $error_log/neowms_tables.err
-psql -d neowms -h 10.5.2.4 -p 5432 -U dblidl -f $tools/build_target_views.sql > $stdout/build_target_views.lst 2> $error_log/build_target_views.err 
-cat $error_log/build_target_views.err
